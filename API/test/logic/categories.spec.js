6804be5404d9884848df1e5c154f4a7667ef7d5c
Bryan <bazofeifa89@gmail.com>
2015-02-14 20:42:10 -0500

    API - Defined the unit testing skeleton for the categories

---
+++ API/test/logic/categories.spec.js
@@ -0,0 +1,47 @@
+'use strict';
+
+describe('Logic/Categories', function () {
+	beforeEach('Initialize db access', function () {
+
+	});
+
+	it('should add a new category', function () {
+
+	});
+
+	it('should return an error if there is a category with the same name', function () {
+
+	});
+
+	it('should return null if the the category doesn\'t exist', function () {
+
+	});
+
+	it('should not allow add category without a name', function () {
+
+	});
+
+	it('should validate that the name is not spaces', function () {
+
+	});
+
+	it('should update a category', function () {
+
+	});
+
+	it('should get all the categories', function () {
+
+	});
+
+	it('should get categories in a range', function () {
+
+	});
+
+	it('should get one category by his id', function () {
+
+	});
+
+	it('delete a category', function () {
+
+	});
+});
---
+++ API/test/routes/categories.spec.js
@@ -0,0 +1,31 @@
+'use strict';
+
+describe('Routes', function () {
+	it('should define the route for GET categories/', function () {
+
+	});
+
+	it('should define the route for GET categories/id', function () {
+
+	});
+
+	it('should return an error if the category doesn\'t exist GET categories/id', function () {
+
+	});
+
+	it('should define the route for GET a range of categories/?start', function () {
+
+	});
+
+	it('should define the route for POST categories/', function () {
+
+	});
+
+	it('should define the route for PUT categories/', function () {
+
+	});
+
+	it('should define the route for DELETE categories/', function () {
+
+	});
+});
