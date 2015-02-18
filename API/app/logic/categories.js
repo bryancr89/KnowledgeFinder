71162092b22536651ffd06306cebc2166b222a5a
Bryan <bazofeifa89@gmail.com>
2015-02-17 21:13:46 -0500

    API - Added basic unit testing for category

--- API/app/logic/categories.js
+++ API/app/logic/categories.js
@@ -1,1 +1,14 @@
 'use strict';
+
+var Category = require('../models/category');
+
+function save(category, cb) {
+	var newCategory = new Category(category);
+	newCategory.save(function(err, data) {
+		cb(null, data);
+	});
+}
+
+module.exports = {
+	save: save
+};
--- API/test/logic/categories.spec.js
+++ API/test/logic/categories.spec.js
@@ -1,12 +1,42 @@
 'use strict';
 
-describe('Logic/Categories', function () {
-	beforeEach('Initialize db access', function () {
+var chai = require('chai');
+var sinon = require('sinon');
+var sinonChai = require("sinon-chai");
+var proxyquire = require('proxyquire');
+var expect = chai.expect;
+chai.use(sinonChai);
 
+describe('Logic/Categories', function () {
+	var mongoose = require('mongoose'),
+		categoryLogic,
+		categoryModel,
+		saveFakeMongo = sinon.stub(),
+		fakeModel = {
+			save: saveFakeMongo
+		};
+
+	before(function () {
+		categoryModel = sinon.stub().returns(fakeModel);
+		categoryLogic = proxyquire('../../.tmp/logic/categories.js', {
+			mongoose: mongoose,
+			'../models/category': categoryModel
+		});
+	});
+
+	beforeEach('Initialize db access', function () {
+		saveFakeMongo = sinon.stub();
 	});
 
 	it('should add a new category', function () {
+		var newCategory = { name: 'test', description: 'A dummy stuff' },
+			callback = sinon.stub();
+		fakeModel.save.yields(null, newCategory);
 
+		categoryLogic.save(newCategory, callback);
+
+		expect(callback).to.have.been.calledWith(null, newCategory);
+		expect(categoryModel).to.have.been.calledWith(newCategory);
 	});
 
 	it('should return an error if there is a category with the same name', function () {
