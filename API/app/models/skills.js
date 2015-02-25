cd15129cdc694c5537380a0d780371c0402f12a8
Bryan <bazofeifa89@gmail.com>
2015-02-24 21:13:04 -0500

    API Defined the model for skills

--- API/app/models/skills.js
+++ API/app/models/skills.js
@@ -1,3 +1,10 @@
-/**
- * Created by bryan on 2/9/15.
- */
+'use strict';
+var mongoose = require('mongoose');
+
+var skillSchema = mongoose.Schema({
+	name: String,
+	description: String,
+	categories: [mongoose.Schema.Types.ObjectId]
+});
+
+module.exports = mongoose.model('skillSchema', skillSchema)
